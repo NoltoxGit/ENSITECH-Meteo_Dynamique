311e9beb364ef4936145a281f069896f0b4c978d
Poy <paulmuller78210@gmail.com>
2025-10-01 14:51:33 +0200

    Ajout de la structure HTML, du script JavaScript et des styles CSS pour l'application météo

---
+++ script.js
@@ -0,0 +1,6 @@
+const searchBtn = document.getElementById("searchBtn");
+const resultDiv = document.getElementById("result");
+
+searchBtn.addEventListener("click", () => {
+ resultDiv.innerHTML = "Tu as cliqué sur le bouton !";
+});
